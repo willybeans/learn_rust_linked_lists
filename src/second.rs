3b939bde2b644bb94aaad08ed62aa04acdced537
Will Wedmedyk <willwedmedyk@gmail.com>
2024-01-17 09:31:45 -0500

    chapter 2:: an ok stack:: learning option

--- src/second.rs
+++ src/second.rs
@@ -1,53 +1,49 @@
 use std::mem;
 
 pub struct List {
-  head: Link,
+    head: Link,
 }
 
-enum Link {
-  Empty,
-  More(Box<Node>),
-}
+// yay type aliases!
+type Link = Option<Box<Node>>;
 
 struct Node {
-  elem: i32,
-  next: Link,
+    elem: i32,
+    next: Link,
 }
 
 impl List {
-  pub fn new() -> Self {
-      List { head: Link::Empty }
-  }
-  pub fn push(&mut self, elem: i32) {
-    let new_node = Box::new(Node {
-        elem: elem,
-        next: mem::replace(&mut self.head, Link::Empty),
-    });
+    pub fn new() -> Self {
+        List { head: None }
+    }
 
-    self.head = Link::More(new_node);
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, None),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
-pub fn pop(&mut self) -> Option<i32> {
-  match mem::replace(&mut self.head, Link::Empty) {
-      Link::Empty => None,
-      Link::More(node) => {
-          self.head = node.next;
-          Some(node.elem)
-      }
-  }
-}
-
-}
-
-
 
 impl Drop for List {
-  fn drop(&mut self) {
-      let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-
-      while let Link::More(mut boxed_node) = cur_link {
-          cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
-      }
-  }
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, None);
+        }
+    }
 }
 
 
